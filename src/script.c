b2fbdbb8e1324aa64d71b85e71bb33b5a3f41b6c
kastetsGitHub <kastets1@gmail.com>
2023-03-19 18:09:29 +0300

    Add dosomthing function

--- src/script.c
+++ src/script.c
@@ -1,9 +1,17 @@
-function Print(string str == "Hello World")
+void function Print(string str == "Hello World")
 {
 	print(str);
 } 
 
-function(string str, int count)
+void function(string str, int count)
 {
 	print($"str is {count}")
+}
+
+void dosomthing()
+{
+	while(true)
+	{
+		print("Happy!");
+	}
 }
