6ba0060b371673f1ad6b9431d8e68abd261ab82d
kastetsGitHub <kastets1@gmail.com>
2023-03-25 13:10:18 +0300

    Add View Program

---
+++ View/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace System.View
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class ConsoleLogWritter
+    {
+        public virtual void WriteError(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    class FileLogWritter
+    {
+        public virtual void WriteError(string message)
+        {
+            File.WriteAllText("log.txt", message);
+        }
+    }
+
+    class SecureConsoleLogWritter : ConsoleLogWritter
+    {
+        public override void WriteError(string message)
+        {
+            if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+            {
+                base.WriteError(message);
+            }
+        }
+    }
+
+    public class Pathfinder
+    {
+        public Pathfinder(Iloger iloger)
+        {
+
+        }
+    }
+
+    public interface Iloger
+    {
+
+    }
+}
